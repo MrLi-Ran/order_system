98fd43f393d0a13d4b6a88c0b068eb6333e11bae
qc <qc@LAPTOP-FJK7LJUE>
2021-03-24 12:01:01 +0800

    QinCan 的提交

---
+++ Git-2/src/test/java/com/example/Git/Git2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
